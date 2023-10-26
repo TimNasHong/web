186cc1daf09cd82d562c1b82547beca9a72f216f
han <2574403276@qq.com>
2023-10-26 20:03:56 +0800

    苍穹代码

---
+++ src/main/java/com/example/skydemo/SkydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.skydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkydemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkydemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skydemo/SkydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
